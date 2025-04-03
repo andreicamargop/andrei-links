cbae7282df62d8d6530e224b72206395e3018d96
andreicamargop <andreicamargopinto2@hotmail.com>
2025-04-03 19:34:46 -0300

    Atualização aula site de links

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { FaGithubSquare } from "react-icons/fa";
+import Linkedin from './components/Linkedin';
+
+
+function App() {
+ 
+
+  return (
+    <div className='container-app'>
+      <h1>Meus Links</h1>
+
+      <a href="https://github.com/y2kyg0r" className='links' target='_blanck'><FaGithubSquare />Github</a>
+      <Linkedin />
+
+    </div>
+  )
+}
+
+export default App
